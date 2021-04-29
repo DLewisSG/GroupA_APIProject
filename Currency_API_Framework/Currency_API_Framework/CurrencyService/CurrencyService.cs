4117c0d337ddc353bbe3120252e27547a4ea1f07
jsahota91 <jsahota@spartaglobal.com>
2021-04-29 13:55:26 +0100

    CurrencyService class updated

--- Currency_API_Framework/Currency_API_Framework/CurrencyService/CurrencyService.cs
+++ Currency_API_Framework/Currency_API_Framework/CurrencyService/CurrencyService.cs
@@ -8,5 +8,35 @@ namespace Currency_API_Framework.CurrencyService
 {
     public class CurrencyService
     {
+        #region Properties
+        public CallManager CallManager { get; }
+        //newtonsoft object for Json response
+        public JObject ResponseContent;
+        //object model of the response
+        public CurrencyDTO CurrencyDTO { get; set; }
+        //currency used in this API Request
+        public string CurrencySelected { get; set; }
+        public string CurrencyResponse { get; set; }
+        #endregion
+
+        //constructor- create Restclient object
+        public CurrencyService()
+        {
+            CallManager = new CallManager();
+            CurrencyDTO = new CurrencyDTO();
+        }
+
+        public async Task MakeRequestAsync(string currency)
+        {
+            CurrencySelected = currency;
+            //Make request
+            CurrencyResponse = await CallManager.MakeCurrencyRequest(currency);
+
+            //parse json into a JObject
+            ResponseContent = JObject.Parse(CurrencyResponse);
+        }
     }
+
+    
+
 }
